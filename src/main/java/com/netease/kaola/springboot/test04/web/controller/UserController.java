5fd27d7fadefbe544b34429dba43913ea71e676a
zouw <2269607500@qq.com>
2018-12-18 23:45:35 +0800

    web开发，主要是静态资源的加载，jquery、Thymeleaf的引入

---
+++ src/main/java/com/netease/kaola/springboot/test04/web/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.netease.kaola.springboot.test04.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.Map;
+
+@Controller
+public class UserController {
+
+    @RequestMapping("/testWeb")
+    public String testWeb() {
+        return "success";
+    }
+
+    @RequestMapping("/success")
+    public String success(Map<String,Object> map){
+        map.put("hello","<h1>你好</h1>");
+        map.put("users", Arrays.asList("zhangsan","lisi","wangwu"));
+        return "success";
+    }
+}
